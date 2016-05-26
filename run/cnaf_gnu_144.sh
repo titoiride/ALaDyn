5d414082b849098be3549bb56a75743aebfa710e
Stefano Sinigardi <stesinigardi@hotmail.com>
2016-05-26 10:32:37 +0100

    upgrade openmpi

--- run/cnaf_gnu_144.sh
+++ run/cnaf_gnu_144.sh
@@ -27,7 +27,7 @@ echo "#BSUB -a openmpi" >> $job
 echo "#BSUB -n ${TOTAL_NUMBER_OF_CORES}" >> $job
 echo "module purge" >> $job
 echo "module load compilers/gcc-4.9.0" >> $job
-echo "module load compilers/openmpi-1.8.1_gcc-4.8.2" >> $job
+echo "module load compilers/openmpi-1.8.1_gcc-4.9.0_with_cuda6.5" >> $job
 echo "module load boost_1_56_0_gcc4_9_0" >> $job
 echo "/usr/share/lsf/9.1/linux2.6-glibc2.3-x86_64/bin/mpirun.lsf env PSM_SHAREDCONTEXTS_MAX=8 ${EXECUTABLE} >> ${stdout_file} 2>> ${stderr_file}" >> $job
 
