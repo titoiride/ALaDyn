405945a9c213ffc3fd3cadfd56b68ddccc000c11
Stefano Sinigardi <stesinigardi@hotmail.com>
2016-10-09 22:21:21 +0200

    force export TMI CONFIG in order to circumvent problems

--- run/cnaf_ifort_144.sh
+++ run/cnaf_ifort_144.sh
@@ -27,6 +27,7 @@ echo "#BSUB -n ${TOTAL_NUMBER_OF_CORES}" >> $job
 echo "module load compilers/gcc-4.9.0" >> $job
 echo "module load compilers/intel-parallel-studio-2016" >> $job
 echo "module load boost_1_56_0_gcc4_9_0" >> $job
+echo "export TMI_CONFIG=/shared/software/compilers/impi/intel64/etc/tmi.conf" >> $job
 echo "/shared/software/compilers/impi/intel64/bin/mpirun -np ${TOTAL_NUMBER_OF_CORES} -genv PSM_SHAREDCONTEXTS_MAX 8 -genv I_MPI_FABRICS shm:tmi ${EXECUTABLE} >> ${stdout_file} 2>> ${stderr_file}" >> $job
 
 
