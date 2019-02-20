f4f199aca2b0a22342b91fa54f506557503d299c
Davide Terzani <titoiride@gmail.com>
2019-02-20 11:49:11 +0100

    Version bumped after PR merge

--- src/code_util.f90
+++ src/code_util.f90
@@ -26,7 +26,7 @@
  implicit none
 
  integer,parameter :: major_version = 7
- integer,parameter :: minor_version = 3
+ integer,parameter :: minor_version = 4
  character(6) :: sw_name='ALaDyn'
  character(9) :: input_namelist_filename='input.nml'
  character(10) :: input_data_filename='input.data'
