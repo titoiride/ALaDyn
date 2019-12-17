1d0c7bb4c953273a8155739f0afa0be7920536c2
titoiride <titoiride@gmail.com>
2019-12-17 10:29:08 +0100

    Imply the proper FFT sine and cosine transform

--- src/fft/mpi_fft_lib.F90
+++ src/fft/mpi_fft_lib.F90
@@ -27,32 +27,74 @@
   implicit none
   include 'fftw3-mpi.f03'
 
-  type(C_PTR) :: plan, data
-  real(C_DOUBLE), pointer :: in(:,:,:)
-  complex(C_DOUBLE_COMPLEX), pointer :: out(:,:,:)
+  !=== Real arrays for the r2r transforms
+  real(C_DOUBLE), pointer :: in1_1d(:)
+  !! Array for the FFT along x
+  real(C_DOUBLE), pointer :: in2_1d(:)
+  !! Array for the FFT along y
+  real(C_DOUBLE), pointer :: in3_1d(:)
+  !! Array for the FFT along z
+  type(C_PTR) :: data1_sc, data2_sc, data3_sc
+  !! C Pointers assigned to the arrays
+
+  !=== Plans that define the sine and cosine transform === 
+  type(C_PTR) :: plan1_s, plan1_c
+  !! Plans of the sine and cosine FFT along x
+  type(C_PTR) :: plan2_s, plan2_c
+  !! Plans of the sine and cosine FFT along y
+  type(C_PTR) :: plan3_s, plan3_c
+  !! Plans of the sine and cosine FFT along z
+
+  !=== Old plans kept for backward compatibility
+  !=== (with the old_ftw1d_sc)
   type(C_PTR) :: plan1, iplan1
   type(C_PTR) :: plan2, iplan2
   type(C_PTR) :: plan3, iplan3
-
+  
+  !=== Old variables
   real(C_DOUBLE), pointer :: w1_re(:), w2_re(:), w3_re(:)
   complex(C_DOUBLE_COMPLEX), pointer :: w1_cplx(:), w2_cplx(:), w3_cplx(:)
+  type(C_PTR) :: data1, data2, data3
   real (dp), allocatable :: cw(:, :), w1_st(:)
   real (dp), allocatable :: cfhx(:), sfhx(:)
   real (dp), allocatable :: cfhy(:), sfhy(:)
   real (dp), allocatable :: cfhz(:), sfhz(:)
-  type(C_PTR) :: data1, data2, data3
 
  contains
 
-  subroutine ftw_mpi_init(L, M, N)
-   integer, intent (in) :: L, M, N
-   data = fftw_alloc_complex(int((L/2+1) * M * N, C_SIZE_T))
-   call c_f_pointer(data, in, [2*(L/2+1),M,N])
-   call c_f_pointer(data, out, [L/2+1,M,N])
-   plan = fftw_plan_dft_r2c_3d(N, M, L, in, out, FFTW_ESTIMATE)
-  end subroutine
+  pure function logical_dimension( kind_flag , N) result(log_dim)
+  !! Returns the logical dimension given the FFT Kind.
+  !! To be generalized for other applications than 
+  !! FFTW_RODFT00 and FFTW_REDFT00
 
+   integer(C_INTPTR_T), intent(in) :: kind_flag
+   integer, intent(in) :: N
+   integer :: log_dim
+   select case(kind_flag)
+   case(FFTW_RODFT00)
+    log_dim = 2*(N + 1)
+   case(FFTW_REDFT00)
+    log_dim = 2*(N - 1)
+   case default
+    log_dim = 2*N
+   end select
+  end function
+
+  pure function determine_kind( sym_index ) result(kind)
+  !! Converts the integer flag `sym` into the proper FFT kind
+   integer, intent(in) :: sym_index
+   integer(C_INTPTR_T) :: kind
+
+   select case(sym_index)
+   case(1)
+    kind = FFTW_RODFT00
+   case(2)
+    kind = FFTW_REDFT00
+   end select
+
+  end function
   subroutine ftw_init(n1, n2, n3, ind_ft)
+   !! Initialization of all the FFT variables and plans
    integer, intent (in) :: n1, n2, n3, ind_ft
    integer :: i, nm
    real (dp) :: wk
@@ -152,9 +194,38 @@
     w3_cplx(:) = 0.0
     plan3 = fftw_plan_dft_r2c_1d( 2*n3, w3_re, w3_cplx, fftw_estimate)
     iplan3 = fftw_plan_dft_c2r_1d( 2*n3, w3_cplx, w3_re, fftw_estimate)
+
+    !===== Trying out new version based on r2r transforms =====
+    data1_sc = fftw_alloc_complex(int( n1, C_SIZE_T))
+    call c_f_pointer(data1_sc, in1_1d, [n1])
+
+    in1_1d(:) = zero_dp
+    plan1_s = fftw_plan_r2r_1d( n1, in1_1d, in1_1d, FFTW_RODFT00, &
+     FFTW_ESTIMATE)
+    plan1_c = fftw_plan_r2r_1d( n1, in1_1d, in1_1d, FFTW_REDFT00, &
+     FFTW_ESTIMATE)
+
+    data2_sc = fftw_alloc_complex(int( n2, C_SIZE_T))
+    call c_f_pointer(data2_sc, in2_1d, [n2])
+
+    in2_1d(:) = zero_dp
+    plan2_s = fftw_plan_r2r_1d( n2, in2_1d, in2_1d, FFTW_RODFT00, &
+     FFTW_ESTIMATE)
+    plan2_c = fftw_plan_r2r_1d( n2, in2_1d, in2_1d, FFTW_REDFT00, &
+     FFTW_ESTIMATE)
+    
+    data3_sc = fftw_alloc_complex(int( n3, C_SIZE_T))
+    call c_f_pointer(data3_sc, in3_1d, [n3])
+
+    in3_1d(:) = zero_dp
+    plan3_s = fftw_plan_r2r_1d( n3, in3_1d, in3_1d, FFTW_RODFT00, &
+     FFTW_ESTIMATE)
+    plan3_c = fftw_plan_r2r_1d( n3, in3_1d, in3_1d, FFTW_RODFT00, &
+     FFTW_ESTIMATE)
+
    end select
   end subroutine
-!----------------------
+
   subroutine ftw_end
    !! Routines that ends all the FFTs. It destroys the existing plans
    !! and deallocates the arrays.
@@ -166,9 +237,18 @@
    call fftw_destroy_plan(iplan1)
    call fftw_destroy_plan(iplan2)
    call fftw_destroy_plan(iplan3)
+   call fftw_destroy_plan(plan1_s)
+   call fftw_destroy_plan(plan2_s)
+   call fftw_destroy_plan(plan3_s)
+   call fftw_destroy_plan(plan1_c)
+   call fftw_destroy_plan(plan2_c)
+   call fftw_destroy_plan(plan3_c)
    call fftw_free(data1)
    call fftw_free(data2)
    call fftw_free(data3)
+   call fftw_free(data1_sc)
+   call fftw_free(data2_sc)
+   call fftw_free(data3_sc)
 
   end subroutine
 
@@ -636,15 +716,118 @@
    integer :: ix, iy, iz, i1, i2, n1_tr, n2_tr
    real (dp) :: sc
    integer :: ndb
+   integer(C_INTPTR_T) :: kind
 
    !!$PRAGMA C( DFFTW_EXECUTE )
+   kind = determine_kind(sym)
+   select case (dir)
+   case (1)
+    in1_1d(:) = zero_dp
+    ndb = logical_dimension( kind, n1+1 )
+    if (is < 0) then !grid to Fourier space
+     sc = 1./real(ndb, dp)
+     if (kind == FFTW_RODFT00) then !sin
+      do iz = 1, n3
+       do iy = 1, n2
+        in1_1d(1:n1) = w(1:n1, iy, iz)
+        call fftw_execute_r2r(plan1_s, in1_1d, in1_1d)
+        w(1:n1, iy, iz) = sc*in1_1d(1:n1)
+       end do
+      end do
+     else if (kind == FFTW_REDFT00) then
+      do iz = 1, n3 !cos
+       do iy = 1, n2
+        in1_1d(1:n1) = w(1:n1, iy, iz)
+        call fftw_execute_r2r(plan1_c, in1_1d, in1_1d)
+        w(1:n1, iy, iz) = sc*in1_1d(1:n1)
+       end do
+      end do
+     end if
+    else
+     if (kind == FFTW_RODFT00) then
+      do iz = 1, n3
+       do iy = 1, n2
+        in1_1d(:) = zero_dp
+        in1_1d(1:n1) = w(1:n1, iy, iz)
+        call fftw_execute_r2r(plan1_s, in1_1d, in1_1d)
+        w(1:n1, iy, iz) = in1_1d(1:n1)
+       end do
+      end do
+     else if (kind == FFTW_REDFT00) then
+      do iz = 1, n3
+       do iy = 1, n2
+        in1_1d(:) = zero_dp
+        in1_1d(1:n1) = w(1:n1, iy, iz)
+        call fftw_execute_r2r(plan1_c, in1_1d, in1_1d)
+        w(1:n1, iy, iz) = in1_1d(1:n1)
+       end do
+      end do
+     end if
+    end if
+   case (2)
+    in2_1d(:) = zero_dp
+    ndb = logical_dimension( kind, n2 )
+    if (is < 0) then
+     sc = 1./real(ndb, dp)
+     do iz = 1, n3
+      do ix = 1, n1
+       in2_1d(1:n2) = w(ix, 1:n2, iz)
+       call fftw_execute_r2r(plan2_s, in2_1d, in2_1d)
+       w(ix, 1:n2, iz) = sc*in2_1d(1:n2)
+      end do
+     end do
+    else
+     do iz = 1, n3
+      do ix = 1, n1
+       in2_1d(:) = zero_dp
+       in2_1d(1:n2) = w(ix, 1:n2, iz)
+       call fftw_execute_r2r(plan2_s, in2_1d, in2_1d)
+       w(ix, 1:n2, iz) = in2_1d(1:n2)
+      end do
+     end do
+    end if
+   case (3)
+    in3_1d(:) = zero_dp
+    ndb = logical_dimension( kind, n3 )
+    if (is < 0) then
+     sc = 1./real(ndb, dp)
+     do iy = 1, n2
+      do ix = 1, n1
+       in3_1d(:) = zero_dp
+       in3_1d(1:n3) = w(ix, iy, 1:n3)
+       call fftw_execute_r2r(plan3_s, in3_1d, in3_1d)
+       w(ix, iy, 1:n3) = sc*in3_1d(1:n3)
+      end do
+     end do
+    else
+     do iy = 1, n2
+      do ix = 1, n1
+       in3_1d(1:n3) = w(ix, iy, 1:n3)
+       call fftw_execute_r2r(plan3_s, in3_1d, in3_1d)
+       w(ix, iy, 1:n3) = in3_1d(1:n3)
+      end do
+     end do
+    end if
+   end select
+  end subroutine
 
+  subroutine old_ftw1d_sc(w, n1, n2, n3, is, dir, sym)
+   real (dp), intent (inout) :: w(:, :, :)
+
+   integer, intent (in) :: n1, n2, n3, is, dir, sym
+   integer :: ix, iy, iz, i1, i2, n1_tr, n2_tr
+   real (dp) :: sc
+   integer :: ndb
+   integer(C_INTPTR_T) :: kind
+
+   !!$PRAGMA C( DFFTW_EXECUTE )
+   kind = determine_kind(sym)
    select case (dir)
    case (1)
     ndb = 2*n1
     if (is<0) then !grid to Fourier space
      sc = 1./real(ndb, dp)
-     if (sym==1) then !sin
+     if (kind == FFTW_RODFT00) then !sin
       do iz = 1, n3
        do iy = 1, n2
         w1_re(1:n1) = w(1:n1, iy, iz)
@@ -658,7 +841,7 @@
         end do
        end do
       end do
-     else
+     else if (kind == FFTW_REDFT00) then
       do iz = 1, n3 !cos
        do iy = 1, n2
         w1_re(1:n1) = w(1:n1, iy, iz)
@@ -675,7 +858,7 @@
      end if
     else
      n1_tr = n1
-     if (sym==1) then
+     if (kind == FFTW_RODFT00) then
       do iz = 1, n3
        do iy = 1, n2
         w1_cplx(:) = 0.0
@@ -686,7 +869,7 @@
         w(1:n1, iy, iz) = w1_re(1:n1)
        end do
       end do
-     else
+     else if (kind == FFTW_REDFT00) then
       do iz = 1, n3
        do iy = 1, n2
         w1_cplx(:) = 0.0
